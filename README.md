12e7761e89551c42d674cbc9bae47096ccf209ab
NingXinyu <nxyqdl@163.com>
2023-03-02 14:19:58 +0800

    add springboot

--- README.md
+++ README.md
@@ -4,15 +4,45 @@ b站课程《从0开始带你手撸一套SpringBoot+Vue后台管理系统（2022
 
 课程地址：[从0开始带你手撸一套SpringBoot+Vue后台管理系统（2022年最新版）](https://www.bilibili.com/video/BV1U44y1W77D?p=5&vd_source=c2132b75ea9167e7d4b07ff05d7cd9c0)
 
+进度说明：目前完成课程P10之前的内容。
+
+## 前端页面展示
+[](./img/index.png)
+
 开发所用软件版本：
-* Java version: 1.8.0_361
+* Jdk: 1.8
 * MySQL: 8.0.32
 * node: v16.17.0
 * Vue CLI: 4.5.14
 * IDEA: 2021.3
 * Element UI: 2.15.13
 * Navicat: 16.1.8
+* postman: 10.10.9
 
-## Vue
 
-Vue 前端工程文件所在目录。
+## Springboot
+
+版本：
+Spring Boot: 2.7.9
+
+依赖：
+* Spring Web / Spring MVC
+* MyBatis Framework
+* MySQL Driver
+* MyBatis-Plus: 3.5.1
+
+前端端口: http://localhost:8080/
+后端端口: http://localhost:8081/
+
+### 数据库设计
+
+数据库名称: qing
+
+表: 
+* sys_user
+
+
+
+### Vue
+
+Vue 前端工程文件所在目录: Springboot/Vue
---
+++ springboot/src/main/java/com/example/springboot/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.springboot.service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.springboot.entity.User;
+import com.example.springboot.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService extends ServiceImpl<UserMapper, User> {
+
+//    @Autowired
+//    private UserMapper userMapper;
+//
+//
+//    public int save(User user) {
+//        if(user.getId() == null) {  // user没有id, 则表示是新增
+//            return userMapper.insert(user);
+//        } else { // 否则为更新
+//            return userMapper.update(user);
+//        }
+//    }
+
+    public boolean saveUser(User user) {
+//        if(user.getId() == null) {
+//            return save(user);  //mybatis-plus提供的方法，表示插入数据
+//        } else {
+//            return updateById(user);
+//        }
+        return saveOrUpdate(user);
+    }
+}
